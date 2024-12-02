0ec676dadf89ab5be4663838d8bcfa9362ac2646
DarlanHenrique <darlan.silva@ice.ufjf.br>
2024-12-02 20:11:48 -0300

    create animal

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,5 +1,17 @@
 import "./Animal.css";
 
-export default function() {
-    return <div className="animal">Animal</div>
+interface AnimalProps {
+    icon: string;
+    name: string;
+    weight: number;
+    extinct: boolean;
+}
+
+export default function({icon, name, weight, extinct} : AnimalProps) {
+    return <div className="animal">
+        <div className="animalItem">{icon}</div>
+        <div className="animalItem"><b>{name}</b></div>
+        <div className="animalItem">{weight} kg</div>
+        <div className="animalItem">{extinct}</div>
+    </div>
 }
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Animal from './Animal'
 function App() {
   return (
       <div className='app'>
-        <Animal />
+        <Animal icon="🦖" name="Dinossaur" weight={530} extinct={true} />
+        <Animal icon="🦁" name="Lion" weight={120} extinct={false} />
       </div>
   )
 }
