ce5aecbea3af1250b791f7bfdfc01cabdd0a78f3
DarlanHenrique <darlan.silva@ice.ufjf.br>
2024-12-05 22:11:39 -0300

    adding children in exhibition

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import Exhibition from './Exhibition'
 function App() {
   return (
       <div className='app'>
-        <Exhibition start={new Date("2024-12-06T08:00:00-03:00")} end={new Date("2024-12-06T12:00:00-03:00")} location='A1' />
-        <Animal icon="🐘" name="Elefante" weight={5400} />
-        <Animal icon="🦖" name="Dinossauro" weight={-1} extinct />
-        <Animal icon="🦒" name="Girafa" weight={800} />
-        <hr />
-        <Exhibition start={new Date("2024-12-06T13:00:00-03:00")} end={new Date("2024-12-06T17:00:00-03:00")} location='B1' />
-        <Animal icon="🦁" name="Leão" weight={190.37} />
-        <Animal icon="🐬" name="Golfinho-do-rio-chinês" weight={67.43} extinct />
+        <Exhibition start={new Date("2024-12-06T08:00:00-03:00")} end={new Date("2024-12-06T12:00:00-03:00")} location='A1'> 
+          <Animal icon="🦅" name="Águia" weight={6.35} />
+          <Animal icon="🐘" name="Elefante" weight={5400} />
+          <Animal icon="🦒" name="Girafa" weight={800} />
+          <Animal icon="🦔" name="Porco-espinho" weight={5.0} />
+        </Exhibition>
+        <Exhibition start={new Date("2024-12-06T13:00:00-03:00")} end={new Date("2024-12-06T17:00:00-03:00")} location='B1'>
+          <Animal icon="🦖" name="Dinossauro" weight={-1} extinct />
+          <Animal icon="🦁" name="Leão" weight={190.37} />
+          <Animal icon="🦏" name="Rinoceronte" weight={2300} />
+        </Exhibition>
+        <Exhibition start={new Date("2024-12-06T18:00:00-03:00")} end={new Date("2024-12-06T22:00:00-03:00")} location='C1'>
+          <Animal icon="🐬" name="Golfinho-do-rio-chinês" weight={67.43} extinct />
+        </Exhibition>
       </div>
   )
 }
--- src/Exhibition.tsx
+++ src/Exhibition.tsx
@@ -1,15 +1,21 @@
 import "./Exhibition.css"
+import { ReactNode } from "react"
 
 interface ExhibitionProps {
     start: Date;
     end: Date;
     location: string;
+    children?: ReactNode;
 }
 
-export default function Exhibition({start, end, location} : ExhibitionProps) {
-    return <div className="exhibition">
-        <div className="location">Local: {location}</div>
-        <div className="start">Início: {start.toLocaleString()}</div>
-        <div className="end">Fim: {end.toLocaleString()}</div>
-    </div>
+export default function Exhibition({start, end, location, children:animals} : ExhibitionProps) {
+    return <>  
+        <div className="exhibition">
+            <div className="location">Local: {location}</div>
+            <div className="start">Início: {start.toLocaleString()}</div>
+            <div className="end">Fim: {end.toLocaleString()}</div>
+            <div className="animals">{animals}</div>
+        </div>
+        <hr />
+    </>
 }
